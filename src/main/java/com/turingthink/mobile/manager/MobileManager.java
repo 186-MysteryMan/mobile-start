fe91924476e743ceb2b84391ffefb7d5b64ef35d
盛攻杰 <765040854@qq.com>
2023-11-21 18:37:17 +0800

    feat:h5一键登录

--- src/main/java/com/turingthink/mobile/manager/MobileManager.java
+++ src/main/java/com/turingthink/mobile/manager/MobileManager.java
@@ -1,23 +1,14 @@
 package com.turingthink.mobile.manager;
 
-import com.aliyun.dypnsapi20170525.models.GetMobileRequest;
-import com.aliyun.dypnsapi20170525.models.GetMobileResponse;
-import com.aliyun.dypnsapi20170525.models.GetMobileResponseBody;
+import com.aliyun.dypnsapi20170525.models.*;
 import com.aliyun.tea.TeaException;
-import com.turingthink.mobile.common.Assert;
-import com.turingthink.mobile.common.execption.CustomException;
 import com.turingthink.mobile.common.sms.SmsProperties;
-import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author GongJie Sheng
@@ -44,6 +35,61 @@ public class MobileManager {
             if (Objects.nonNull(mobileWithOptions.getBody())
                     && Objects.nonNull(mobileWithOptions.getBody().getGetMobileResultDTO())) {
                 return mobileWithOptions.getBody().getGetMobileResultDTO().getMobile();
+            }
+        } catch (TeaException error) {
+            // 如有需要，请打印 error
+            com.aliyun.teautil.Common.assertAsString(error.message);
+        } catch (Exception error) {
+            TeaException teaError = new TeaException(error.getMessage(), error);
+            // 如有需要，请打印 error
+            com.aliyun.teautil.Common.assertAsString(teaError.message);
+        }
+        return null;
+    }
+
+    public GetAuthTokenResponseBody.GetAuthTokenResponseBodyTokenInfo getH5Token(String url, String origin) throws Exception {
+        // 请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID 和 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
+        // 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例使用环境变量获取 AccessKey 的方式进行调用，仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378657.html
+        String accessKeyId = smsProperties.getAliyun().getAccessKeyId();
+        String accessKeySecret = smsProperties.getAliyun().getAccessKeySecret();
+        com.aliyun.dypnsapi20170525.Client client = MobileManager.createClient(accessKeyId, accessKeySecret);
+        com.aliyun.dypnsapi20170525.models.GetAuthTokenRequest getAuthTokenRequest = new com.aliyun.dypnsapi20170525.models.GetAuthTokenRequest()
+                .setUrl(url)
+                .setOrigin(origin);
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        try {
+            // 复制代码运行请自行打印 API 的返回值
+            GetAuthTokenResponse authTokenWithOptions = client.getAuthTokenWithOptions(getAuthTokenRequest, runtime);
+            if (Objects.nonNull(authTokenWithOptions.getBody())
+                    && Objects.nonNull(authTokenWithOptions.getBody().getTokenInfo())) {
+                return authTokenWithOptions.getBody().getTokenInfo();
+            }
+        } catch (TeaException error) {
+            // 如有需要，请打印 error
+            com.aliyun.teautil.Common.assertAsString(error.message);
+        } catch (Exception error) {
+            TeaException teaError = new TeaException(error.getMessage(), error);
+            // 如有需要，请打印 error
+            com.aliyun.teautil.Common.assertAsString(teaError.message);
+        }
+        return null;
+    }
+
+
+    public VerifyPhoneWithTokenResponseBody.VerifyPhoneWithTokenResponseBodyGateVerify h5VerifyPhoneWithToken(String phone, String spToken) throws Exception {
+        String accessKeyId = smsProperties.getAliyun().getAccessKeyId();
+        String accessKeySecret = smsProperties.getAliyun().getAccessKeySecret();
+        com.aliyun.dypnsapi20170525.Client client = MobileManager.createClient(accessKeyId, accessKeySecret);
+        com.aliyun.dypnsapi20170525.models.VerifyPhoneWithTokenRequest verifyPhoneWithTokenRequest = new com.aliyun.dypnsapi20170525.models.VerifyPhoneWithTokenRequest()
+                .setPhoneNumber(phone)
+                .setSpToken(spToken);
+        com.aliyun.teautil.models.RuntimeOptions runtime = new com.aliyun.teautil.models.RuntimeOptions();
+        try {
+            // 复制代码运行请自行打印 API 的返回值
+            VerifyPhoneWithTokenResponse verifyPhoneWithTokenResponse = client.verifyPhoneWithTokenWithOptions(verifyPhoneWithTokenRequest, runtime);
+            if (Objects.nonNull(verifyPhoneWithTokenResponse.getBody())
+                    && Objects.nonNull(verifyPhoneWithTokenResponse.getBody().getGateVerify())) {
+                return verifyPhoneWithTokenResponse.getBody().getGateVerify();
             }
         } catch (TeaException error) {
             // 如有需要，请打印 error
